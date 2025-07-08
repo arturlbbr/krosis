826ff063be02457d1173e590133e8f000ad6b23b
art <arturlbbr@gmail.com>
2025-07-07 23:29:17 -0600

    Parsing progression (need GET calls, response code and port still)

--- src/log_parser.py
+++ src/log_parser.py
@@ -11,14 +11,17 @@ def parse_log_line(line):
         dict: A dictionary containing 'timestamp', 'level', and 'message'.
     """
     # Regular expression to match the log format
-    pattern = r''
+    #Try condensing later (like \d+\.{3} or something) and add parsing for GET calls tomorrow
+    pattern = r'(\d+\.\d+\.\d+\.\d+)((\d{1,2}\/){2}\d{2})((\d{2}\:){2}\d{2})(\+\d{4})()'
     match = re.match(pattern, line)
     
     if match:
         return {
-            'timestamp': match.group('timestamp'),
-            'level': match.group('level'),
-            'message': match.group('message')
+            'src_ip': match.group(1),
+            'timestamp': match.group(2),
+            'GET_call': match.group(3),
+            'response_code': match.group(4),
+            'port': match.group(5)
         }
     else:
         return None
